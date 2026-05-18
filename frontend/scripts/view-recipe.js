660b5aa49c48084a72882dacc85be06deb31474d
PEKKA367 <ilya.boogai@gmail.com>
2026-05-18 13:48:39 +0300

    Fixed view recipe HTML

--- frontend/scripts/view-recipe.js
+++ frontend/scripts/view-recipe.js
@@ -1,20 +1,22 @@
 //IMPORTS
-import {db} from './firebase-config.js';
+import {db} from "./firebase-config.js";
 import {doc, getDoc} from "firebase/firestore";
 
 
 //CONSTANTS
 const urlParams = new URLSearchParams(window.location.search);
-const recipeId = urlParams.get('id');
+const recipeId = urlParams.get("id");
 
 const docRef = doc(db, "recipes", recipeId);
 
 //DOM elements
-const titleElement = document.querySelector('.recipe-title');
-const descElement = document.querySelector('.recipe-description');
-const processElement = document.querySelector('.process');
-const ingredientsList = document.querySelector('.ingredients-list');
-const template = document.getElementById('ingredient-and-amount-template');
+const titleElement = document.querySelector(".recipe-title");
+const descElement = document.querySelector(".recipe-description");
+const stepsProcessList = document.querySelector(".steps-list");
+const ingredientsList = document.querySelector(".ingredients-list");
+const ingredientTemplate = document.getElementById("ingredient-and-amount-template");
+const stepTemplate = document.getElementById("step-process-template");
+
 
 
 //STATE
@@ -24,13 +26,26 @@ const template = document.getElementById('ingredient-and-amount-template');
 function renderIngredients(ingredientsArray) {
     if (!ingredientsArray) return;
 
-    ingredientsList.innerHTML = '';
+    ingredientsList.innerHTML = "";
 
     ingredientsArray.forEach(item => {
-        const clone = template.content.cloneNode(true);
-        clone.querySelector('.ingredient').textContent = item.name;
-        clone.querySelector('.amount').textContent = item.amount;
+        const clone = ingredientTemplate.content.cloneNode(true);
+        clone.querySelector(".ingredient").textContent = item.name;
+        clone.querySelector(".amount").textContent = item.amount;
         ingredientsList.appendChild(clone);
+    });
+}
+
+function renderSteps(stepsArray) {
+    if (!stepsArray) return;
+
+    stepsProcessList.innerHTML = "";
+
+    stepsArray.forEach((item, index) => {
+        const clone = stepTemplate.content.cloneNode(true);
+        clone.querySelector(".step-number").textContent = "Крок " + (index + 1);
+        clone.querySelector(".step-text").textContent = item;
+        stepsProcessList.appendChild(clone);
     });
 }
 
@@ -49,9 +64,9 @@ async function displayRecipeDetails() {
 
         titleElement.textContent = data.title;
         descElement.textContent = data.description;
-        processElement.textContent = data.process;
 
         renderIngredients(data.ingredients);
+        renderSteps(data.steps);
 
     } catch (error) {
         console.error("Помилка:", error);
