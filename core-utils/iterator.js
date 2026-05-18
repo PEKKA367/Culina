719f6913acf1853361228f62a375e1158e4cbd2d
PEKKA367 <ilya.boogai@gmail.com>
2026-05-18 14:09:59 +0300

    Starting to add new generator

--- core-utils/iterator.js
+++ core-utils/iterator.js
@@ -1,39 +1,22 @@
-function* CarouselGenerator() {
-    const words = ["a", "b", "c", "d", "e"];
+function* recipeGenerator(recipesArray) {
     let index = 0;
 
     while (true) {
-
-        yield words[index];
-        index++;
-        if (words.length === index) {
-            index = 0;
-        }
+        yield recipesArray[index];
+        index = (index + 1) % recipesArray.length;
     }
 }
 
 function spinTheCarousel(iterator, durationInSeconds) {
-    const wordElement = document.getElementById('dynamic-word');
-    const button = document.getElementById('spin-btn');
-
-    button.disabled = true;
-
     const startTime = Date.now();
     const durationMs = durationInSeconds * 1000;
 
     const intervalId = setInterval(() => {
         if (Date.now() - startTime >= durationMs) {
             clearInterval(intervalId);
-            button.disabled = false;
             return;
         } else {
-            wordElement.innerText = iterator.next().value;
         }
 
     }, 900);
 }
-
-document.getElementById('spin-btn').addEventListener('click', () => {
-    const controller = CarouselGenerator();
-    spinTheCarousel(controller, 5);
-});
