ea6b74cec8731f4b74c0d928550bcfa6a26e0e0f
PEKKA367 <ilya.boogai@gmail.com>
2026-05-22 04:18:01 +0300

    Added functionality to the front, didnt fix all bugs

--- core-utils/index.js
+++ core-utils/index.js
@@ -1,4 +1,5 @@
-export { recipeGenerator, spinTheCarousel } from './iterator.js';
-export { memoize } from './memoize.js';
-export { BiPriorityQueue } from './BiPriorityQueue.js';
-export { asyncFilter, asyncFilterCb } from './async-filter.js';
+export { recipeGenerator, spinTheCarousel } from "./iterator.js";
+export { memoize } from "./memoize.js";
+export { BiPriorityQueue } from "./BiPriorityQueue.js";
+export { asyncFilter, asyncFilterCb } from "./async-filter.js";
+export { Emitter } from "./Emitter.js"
